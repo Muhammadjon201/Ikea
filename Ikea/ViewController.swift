ba571f29fd2dbec6b24440b72b0b9275f5cdbf49
Muhammadjon <20mamarasulov@gmail.com>
2022-04-01 00:26:59 +0900

    Initial Commit

---
+++ Ikea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ikea
+//
+//  Created by Muhammadjon Mamarasulov on 4/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
