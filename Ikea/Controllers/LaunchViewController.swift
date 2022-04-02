486764a8a26e4cc3438d8a302398a317848bd02c
Muhammadjon <20mamarasulov@gmail.com>
2022-04-03 03:29:43 +0900

    --

---
+++ Ikea/Controllers/LaunchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchViewController.swift
+//  Ikea
+//
+//  Created by Muhammadjon Mamarasulov on 4/1/22.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
